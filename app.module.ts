390a8b68e1b47db5abdf89117048a1a03d9b2f47
paramesh7842 <105286595+paramesh7842@users.noreply.github.com>
2022-05-13 11:24:27 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftsidebarComponent } from './leftsidebar/leftsidebar.component';
+import { FormsModule } from '@angular/forms';
+import { FooterComponent } from './footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    LeftsidebarComponent,
+    FooterComponent,
+
+   
+
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
